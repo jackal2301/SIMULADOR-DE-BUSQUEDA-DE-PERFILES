b995d97e1a73b13c152332bae1db14d00fae549c
jackal2301 <joser16torres@gmail.com>
2020-09-10 18:01:46 -0500

    se sube el index

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+  $mysqli = new mysqli('localhost', 'root', '', 'banco_instructores');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Demo de menú desplegable</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+</head>
+<body>
+<h1>SIMULADOR DE BUSQUEDA DE PERFILES APE</h1>
+  <div>                        
+    <p>Seleccione una Regional del siguiente menú:</p>
+    <p>Regional:
+      <select id="lista1">
+        <option value="0">Seleccione:</option>
+        <?php
+          $query = $mysqli -> query ("SELECT DISTINCT Cod_Centro, Regional FROM centro_prog");
+          while ($valores = mysqli_fetch_array($query)) {
+            echo '<option  "value="'.$valores[Cod_Centro].'">'.$valores[Regional].'</option>';
+          }
+        ?>
+      </select>
+<div>	 
+	<p>Seleccione el sector del siguiente menú:</p>
+    <p>Sector:
+      <select id="lista2">
+		<option value="0">Seleccione:</option>
+        <?php
+          $query = $mysqli -> query ("SELECT DISTINCT Red FROM centro_prog");
+          while ($valores = mysqli_fetch_array($query)) {
+            echo '<option  "value="'.$valores[Cod_Centro].'">'.$valores[Red].'</option>';
+          }
+        ?>
+      </select>
+</div>
+
+<button>Consultar</button>
+  
+  </div>
+</body>
+
+</html>
+
+
+
